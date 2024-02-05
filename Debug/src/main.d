961d519f08e17cfd0e213699b47c00da69096580
Michael Samy <michaelsamy196@gmail.com>
2024-02-06 00:14:06 +0200

    test commit

--- Debug/src/main.d
+++ Debug/src/main.d
@@ -1,5 +1,8 @@
-src/main.o: ../src/main.c ../src/LIB/BIT_MATH.h ../src/LIB/STD_TYPES.h
+src/main.o: ../src/main.c ../src/LIB/BIT_MATH.h ../src/LIB/STD_TYPES.h \
+ ../src/MCAL/MGPIO/MGPIO_interface.h
 
 ../src/LIB/BIT_MATH.h:
 
 ../src/LIB/STD_TYPES.h:
+
+../src/MCAL/MGPIO/MGPIO_interface.h:
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "LIB/BIT_MATH.h"
 #include "LIB/STD_TYPES.h"
+#include "MCAL/MGPIO/MGPIO_interface.h"
 
 int main()
 {
