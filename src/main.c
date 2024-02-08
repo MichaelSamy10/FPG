7f5b9397a84954406dde10fecd91c2403f856ba3
Michael Samy <michaelsamy196@gmail.com>
2024-02-08 09:04:18 +0200

    edit main

--- src/main.c
+++ src/main.c
@@ -8,26 +8,6 @@
 
 int main()
 {
-	/* Init System Clock */
-	MRCC_voidInitializeSystemClock();
-	/* Enable PORTA Clock */
-	MRCC_voidEnablePeripheralClock(MRCC_AHB1,MRCC_GPIOA_EN);
-	/* Init Systick*/
-	MSTK_voidIntialize();
-
-	MGPIO_voidSetPinMode(MGPIO_u8PORTA,0,MGPIO_u8OUTPUT);
-	MGPIO_voidSetPinMode(MGPIO_u8PORTA,1,MGPIO_u8OUTPUT);
-	MGPIO_voidSetPinMode(MGPIO_u8PORTA,2,MGPIO_u8OUTPUT);
-	MGPIO_voidSetPinMode(MGPIO_u8PORTA,4,MGPIO_u8OUTPUT);
-	MGPIO_voidSetPinMode(MGPIO_u8PORTA,5,MGPIO_u8OUTPUT);
-	MGPIO_voidSetPinMode(MGPIO_u8PORTA,6,MGPIO_u8OUTPUT);
-	MGPIO_voidSetPinMode(MGPIO_u8PORTA,7,MGPIO_u8OUTPUT);
-
-	CLCD_voidInit();
-	CLCD_voidMoveCursor(0,7);
-	CLCD_voidSendString("FPG");
-	CLCD_voidMoveCursor(1,5);
-	CLCD_voidSendString("PROJECT");
 
 	while(1)
 	{
