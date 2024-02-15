37c50ebff02bff59c2526774634554b4b8b6daf6
Maghraby Store <aya532885@gmail.com>
2024-02-15 10:55:02 +0200

    I2C

---
+++ src/HAL/I2C/I2C_register.h
@@ -0,0 +1,52 @@
+/*
+ * I2C_register.h
+ *
+ *  Created on: Feb 14, 2024
+ *      Author: Maghraby Store
+ */
+
+#ifndef HAL_I2C_I2C_REGISTER_H_
+#define HAL_I2C_I2C_REGISTER_H_
+
+/*
+ * I2C_register.h
+ *
+ *  Created on: Feb 14, 2024
+ *      Author: Maghraby Store
+ */
+
+#ifndef HAL_I2C_I2C_REGISTER_H_
+#define HAL_I2C_I2C_REGISTER_H_
+
+#define I2C_CR1    *((volatile (u8*))(0x00)
+#define I2C_CR2    *((volatile (u8*))(0x04)
+#define I2C_OAR1   *((volatile (u8*))(0x08)
+#define I2C_OAR2   *((volatile (u8*))(0x0C)
+#define I2C_DR     *((volatile (u8*))(0x10)
+#define I2C_SR1    *((volatile (u8*))(0x14)
+#define I2C_SR2    *((volatile (u8*))(0x18)
+#define I2C_CCR    *((volatile (u8*))(0x1C)
+#define I2C_FLTR   *((volatile (u8*))(0x20)
+#define I2C_TRISE   *((volatile (u8*))(0x2C)
+
+
+
+
+
+
+
+
+
+
+
+#endif /* HAL_I2C_I2C_REGISTER_H_ */
+
+
+
+
+
+
+
+
+
+#endif /* HAL_I2C_I2C_REGISTER_H_ */
