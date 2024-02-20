f1436ee240c17009de762c0b0c7d2ac7d13f2054
muhammed sherif <muhsherif17@gmail.com>
2024-02-20 21:03:46 +0200

    ADD TIMER 1

---
+++ src/MCAL/MTIM1/MTIM1_private.h
@@ -0,0 +1,83 @@
+/*
+Author		: Muhammed Sherif Attia
+date		: 18/2/2024
+version		: 1.0.0
+description : MTIM1_private.h -> Private data File
+peri		: for the developer in MCAL only not for the user
+*/
+
+#ifndef MCAL_MTIM1_MTIM1_PRIVATE_H_
+#define MCAL_MTIM1_MTIM1_PRIVATE_H_
+
+
+#define 	CR1_CEN				0
+/**************************************/
+
+#define     CCRM1_OC2M_15		15
+#define     CCRM1_OC2M_14		14
+#define     CCRM1_OC2M_13		13
+#define     CCRM1_OC2M_12		12
+#define     CCRM1_IC2PSC3		11
+#define     CCRM1_IC2PSC2		10
+#define     CCRM1_CC2S1			9
+#define     CCRM1_CC2S0			8
+#define		CCRM1_OC1M_7		7
+#define     CCRM1_OC1M_6		6
+#define     CCRM1_OC1M_5		5
+#define     CCRM1_OC1M_4		4
+#define     CCRM1_IC1PSC3		3
+#define     CCRM1_IC1PSC2		2
+#define     CCRM1_CC1S1			1
+#define 	CCRM1_CC1S0			0
+
+/************************************/
+
+#define     CCRM2_OC4M_15		15
+#define     CCRM2_OC4M_14		14
+#define     CCRM2_OC4M_13		13
+#define     CCRM2_OC4M_12		12
+#define     CCRM2_IC4PSC3		11
+#define     CCRM2_IC4PSC2		10
+#define     CCRM2_CC4S1			9
+#define     CCRM2_CC4S0			8
+#define		CCRM2_OC3M_7		7
+#define     CCRM2_OC3M_6		6
+#define     CCRM2_OC3M_5		5
+#define     CCRM2_OC3M_4		4
+#define     CCRM2_IC3PSC3		3
+#define     CCRM2_IC3PSC2		2
+#define     CCRM2_CC3S1			1
+#define 	CCRM2_CC3S0			0
+
+/********************************/
+#define    DIER_CC1IE				1
+#define    DIER_CC2IE				2
+#define    DIER_CC3IE				3
+#define    DIER_CC4IE				4
+/************************************/
+#define		CCER_CC1E			0
+#define		CCER_CC1P			1
+#define		CCER_CC1NP			3
+#define      CCER_CC2E			4
+#define      CCER_CC2P			5
+#define      CCER_CC2NP			7
+#define		CCER_CC3E			8
+#define		CCER_CC3P			9
+#define		CCER_CC3NP			11
+#define      CCER_CC4E			12
+#define      CCER_CC4P			13
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* MCAL_MTIM1_MTIM1_PRIVATE_H_ */
